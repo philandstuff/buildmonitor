48d110aab38e0881547589cb9c9d3b7bc7abca9d
Philip Potter <philipgpotter@gmail.com>
2011-02-03 23:30:43 +0530

    three labels

--- src/main/com/thoughtworks/go/monitor/BlameMonitor.java
+++ src/main/com/thoughtworks/go/monitor/BlameMonitor.java
@@ -12,19 +12,30 @@ import java.awt.*;
  */
 public class BlameMonitor implements BuildMonitorListener {
     private JFrame frame;
-    private JLabel label;
+    private JLabel userLabel;
+    private JLabel statusLabel;
+    private JLabel emailLabel;
 
     public void createWindow() {
         frame = new JFrame("BlameMonitor");
-        label = new JLabel("Blame Monitor");
-        frame.getContentPane().add(label);
+        userLabel = new JLabel("Blame Monitor");
+        statusLabel = new JLabel("Blame Monitor");
+        emailLabel = new JLabel("Blame Monitor");
+        frame.getContentPane().setLayout(new GridLayout(3,1));
+        frame.getContentPane().add(userLabel);
+        frame.getContentPane().add(emailLabel);
+        frame.getContentPane().add(statusLabel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setExtendedState(frame.getExtendedState()|JFrame.MAXIMIZED_BOTH);
 
-        label.setFont(new Font("Dialog",Font.PLAIN, 100));
-        label.setOpaque(false);
+        userLabel.setFont(new Font("Dialog",Font.PLAIN, 100));
+        userLabel.setOpaque(false);
+        statusLabel.setFont(new Font("Dialog",Font.PLAIN, 100));
+        statusLabel.setOpaque(false);
+        emailLabel.setFont(new Font("Dialog",Font.PLAIN, 100));
+        emailLabel.setOpaque(false);
         frame.setVisible(true);
 
     }
@@ -33,22 +44,28 @@ public class BlameMonitor implements BuildMonitorListener {
         Container container = frame.getContentPane();
         container.setBackground(Color.RED);
         showUser(user);
+        statusLabel.setText("Broke the build");
     }
 
     public void fixedTheBuild(String user) {
         Container container = frame.getContentPane();
         container.setBackground(Color.GREEN);
         showUser(user);
+        statusLabel.setText("Fixed the build");
     }
 
     public void pushedWorkingBuild(String user) {
         Container container = frame.getContentPane();
         container.setBackground(Color.GREEN);
         showUser(user);
+        statusLabel.setText("Pushed");
     }
 
     private void showUser(String user) {
-        label.setText(user);
+        String[] splits = user.split("[<>]");
+        for (String split : splits) { System.out.println(split); }
+        userLabel.setText(splits[0]);
+        emailLabel.setText(splits[1]);
     }
 
 
