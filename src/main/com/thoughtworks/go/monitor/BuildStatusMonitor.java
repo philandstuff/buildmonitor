107a1c1389d65897e31331c9698a1faf340838d4
Philip Potter <philipgpotter@gmail.com>
2011-02-13 21:23:09 +0530

    Three BlameDisplays, add title to BlameDisplay

--- src/main/com/thoughtworks/go/monitor/BuildStatusMonitor.java
+++ src/main/com/thoughtworks/go/monitor/BuildStatusMonitor.java
@@ -7,21 +7,25 @@ public class BuildStatusMonitor {
     private static final String goServer = "twu-ci";
 
     public static void main(String[] args) throws InterruptedException {
-        BlameDisplay buildDisplay = new BlameDisplay();
-        BlameDisplay wordpressDisplay = new BlameDisplay();
+        BlameDisplay buildDisplay = new BlameDisplay("Build");
+        BlameDisplay deployDisplay = new BlameDisplay("Deploy to staging");
+        BlameDisplay wordpressDisplay = new BlameDisplay("Deploy Wordpress");
 
         JFrame frame = createFrame();
-        frame.setLayout(new GridLayout(2,1));
-        frame.add(buildDisplay.createWindow());
-        frame.add(wordpressDisplay.createWindow());
+        frame.setLayout(new GridLayout(3,1));
+        frame.add(buildDisplay.getPanel());
+        frame.add(deployDisplay.getPanel());
+        frame.add(wordpressDisplay.getPanel());
         frame.setVisible(true);
 
         StageMonitor buildMonitor = new StageMonitor("all", "build", goServer, buildDisplay, new SongListener());
+        StageMonitor deployMonitor = new StageMonitor("all", "deploy-staging", goServer, deployDisplay, new SongListener());
         StageMonitor wordpressMonitor = new StageMonitor("wordpress-cms", "manual-deploy-to-staging-server", goServer, wordpressDisplay, new SongListener());
 
         while (true) {
             try {
                 buildMonitor.pollForNewCompletion();
+                deployMonitor.pollForNewCompletion();
                 wordpressMonitor.pollForNewCompletion();
                 Thread.sleep(10000);
                 System.out.println("Polling...");
